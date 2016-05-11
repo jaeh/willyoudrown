24e2d441c2999954da75661f80c8716ef751d44e
Jascha Ehrenreich <jascha@jaeh.at>
2016-05-11 15:53:13 +0200

    fix, has only been years...

--- willyoudrown.js
+++ willyoudrown.js
@@ -1,11 +1,12 @@
 setInterval( function() {
-	var tasks = document.getElementsByTagName('a')
-	  , chore, evt;
+	var tasks = document.getElementsByTagName('a'),
+      evt = document.createEvent("HTMLEvents"),
+      chore;
 
 	for ( chore in tasks ) {
-		if ( tasks.hasOwnProperty(chore) && element.dispatchEvent ) {
+		if ( tasks.hasOwnProperty(chore) && tasks[chore].dispatchEvent ) {
 			evt = document.createEvent("HTMLEvents");
-			evt.initEvent('click', true, true ); // event type,bubbling,cancelable
+			evt.initEvent('click', true, true ); // event type, bubbling, cancelable
 			tasks[chore].dispatchEvent(evt);
 		}
 	}
